40f1b21a377c85fd324eb07e0112487f41b3d5a8
Dasha Zhukava <dashyl321@mail.ru>
2016-10-21 12:03:53 +0300

    Implemented MVC

--- RequirementsScheduler/src/RequirementsScheduler/Controllers/HomeController.cs
+++ RequirementsScheduler/src/RequirementsScheduler/Controllers/HomeController.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace RequirementsScheduler.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
         {
             return View();
         }
