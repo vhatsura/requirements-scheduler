6686ef11fbbf1fac6eef9399d352e17086014e66
Vadzim Hatsura <vadimgatsura@gmail.com>
2017-05-26 00:04:27 +0300

    * Fixed error with submitted property

--- RequirementsScheduler/RequirementsScheduler/Client/app/components/user-detail/user-detail.component.ts
+++ RequirementsScheduler/RequirementsScheduler/Client/app/components/user-detail/user-detail.component.ts
@@ -29,6 +29,7 @@ import { CustomValidators } from 'ng2-validation';
 export class UserDetailComponent extends GtExpandedRow<any> implements OnInit {
     
     f: FormGroup;
+    submitted = false;
 
     constructor(private formBuilder: FormBuilder) { super(); }
 
@@ -38,18 +39,6 @@ export class UserDetailComponent extends GtExpandedRow<any> implements OnInit {
            expTime: ['', Validators.required],
            signKey: ['FB8A0C93-FE6C-4EA4-9869-81F17A1CF917', Validators.required],
            cloudUrl: ['', Validators.required]
-            // testsAmount: ['', Validators.compose([Validators.required, CustomValidators.min(1)])],
-            // requirementsAmount: ['', Validators.compose([Validators.required, CustomValidators.min(1)])],
-            // n1: ['', Validators.compose([Validators.required, CustomValidators.range([0, 100])])],
-            // n2: ['', Validators.compose([Validators.required, CustomValidators.range([0, 100])])],
-            // n12: ['', Validators.compose([Validators.required, CustomValidators.range([0, 100])])],
-            // n21: ['', Validators.compose([Validators.required, CustomValidators.range([0, 100])])],
-            // minBoundaryRange: ['', Validators.compose([Validators.required, CustomValidators.min(0)])],
-            // maxBoundaryRange: ['', Validators.compose([Validators.required, CustomValidators.min(0)])],
-            // minPercentageFromA: ['', Validators.compose([Validators.required, CustomValidators.range([5, 50])])],
-            // maxPercentageFromA: ['', Validators.compose([Validators.required, CustomValidators.range([5, 50])])],
-            // borderGenerationType: ['', Validators.required],
-            // pGenerationType: ['', Validators.required]
-        }); 
+        });
     }
 }
