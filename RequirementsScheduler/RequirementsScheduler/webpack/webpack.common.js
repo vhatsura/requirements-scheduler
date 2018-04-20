47fa52400af2e5484304b558d6e6687235cc6a11
Vadim Hatsura <vadimgatsura@gmail.com>
2018-04-20 07:32:46 +0300

    commit

--- RequirementsScheduler/RequirementsScheduler/webpack/webpack.common.js
+++ RequirementsScheduler/RequirementsScheduler/webpack/webpack.common.js
@@ -17,7 +17,7 @@ module.exports = {
         { test: /\.ts$/, loader: '@ngtools/webpack' },
         { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
         { test: /\.html$/, loader: 'html-loader' },
-        { test: /\.scss$/, loaders: [{loader: "style-loader" }, { loader: "css-loader"}, { loader: "sass-loader"}] },
+        { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
         { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url-loader?limit=10000' }
     ]
   },
